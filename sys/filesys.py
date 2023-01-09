18b970184287110459d51580bd1ee66733361608
nate_4000 <nathanieltebo8@gmail.com>
2023-01-09 13:07:00 -0800

    added nonrelative cd'ing

--- sys/filesys.py
+++ sys/filesys.py
@@ -11,22 +11,38 @@ trdir = os.getcwd().replace('\\',"/")
 
 cwd = rdir
 
-def flop(fn):
-    file = open(trdir + fn)
-    return file
+def uflop(fn):
+    f = open(trdir + fn)
+    return f
 
 def cd(path):
     global cwd
-    if os.path.exists(path):
-        try:
-            os.chdir(path)
-            cwd = os.getcwd()
-            cwd = cwd.replace('\\',"/")
-            cwd = cwd.removeprefix(trdir)
-        except: # you lied to me
-            print("directory is file")
+    if path.startswith("/"):
+        # raise NotImplementedError
+        os.chdir(trdir)
+        cwd = "/"
+        if path == "/":
+            return
+        lpath = path.split("/")
+        print("attempting cd tree " + str(lpath))
+        for t in lpath:
+            if t == '':
+                continue
+            try:
+                cd(t)
+            except:
+                print("unable to open directory " + t)
     else:
-        print("directory not found")
+        if os.path.exists(path):
+            try:
+                os.chdir(path)
+                cwd = os.getcwd()
+                cwd = cwd.replace('\\',"/")
+                cwd = cwd.removeprefix(trdir)
+            except: # you lied to me
+                print("directory is file")
+        else:
+            print("directory not found")
 
 def ls():
     return os.listdir()
